a6074d01548c192f0dbef89ce8cca9e147368a1a
qwqc <lz@live.de>
2025-03-14 15:07:56 +0100

    buttons on MP done
    next step time

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -85,25 +85,7 @@ namespace SideHub
 
         }
 
-        public class MultiplyConverter : IMultiValueConverter
-        {
-            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-            {
-                double result = 1.0;
-                for (int i = 0; i < values.Length; i++)
-                {
-                    if (values[i] is double)
-                        result *= (double)values[i];
-                }
 
-                return result;
-            }
-
-            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-            {
-                throw new Exception("Not implemented");
-            }
-        }
 
 
 
